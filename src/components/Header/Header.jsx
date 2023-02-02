325470e1c6104cec96d8f91d14ddd9371520bbb8
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-02 19:55:05 -0300

    Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
-import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "../HeaderOption/HeaderOption";
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SearchIcon from '@mui/icons-material/Search';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import MessageIcon from '@mui/icons-material/Message';
+import WorkIcon from '@mui/icons-material/Work';
 
 const Header = () => {
   return (
@@ -16,7 +22,12 @@ const Header = () => {
         </div>
       </div>
       <div className="header__right">
-        
+        <HeaderOption Icon={HomeIcon} title={"Home"} />
+        <HeaderOption Icon={SupervisorAccountIcon} title={"My network"} />
+        <HeaderOption Icon={WorkIcon} title={"Jobs"} />
+        <HeaderOption Icon={MessageIcon} title={"Messaging"} />
+        <HeaderOption Icon={NotificationsNoneIcon} title={"Notifications"} />
+        <HeaderOption avatar="https://avatars.githubusercontent.com/u/51086021?v=4" title="me" />
       </div>
     </div>
   );
